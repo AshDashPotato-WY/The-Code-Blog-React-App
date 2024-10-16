3bff0d19fab6cda42f3f960403cb14d03989c773
AshDashPotato-WY <ashleyweiyu@yahoo.com>
2024-10-16 15:38:50 -0400

    Conditioanl loading messages & handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -14,13 +14,15 @@ const Home = () => {
   //   ]);
 
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const [name, setName] = useState("mario");
+  //   const [name, setName] = useState("mario");
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  //   const handleDelete = (id) => {
+  //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //     setBlogs(newBlogs);
+  //   };
 
   //   function fires on every render
   //   useEffect(() => {
@@ -29,23 +31,38 @@ const Home = () => {
   //   }, [name]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            // error comes back from the server
+            throw Error("Can't fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       {/* <BlogList
         blogs={blogs.filter((blogs) => blogs.author === "mario")}
         title="Mario's Blogs"
       /> */}
-      <button onClick={() => setName("luigi")}>change name</button>
+      {/* <button onClick={() => setName("luigi")}>change name</button> */}
     </div>
   );
 };
